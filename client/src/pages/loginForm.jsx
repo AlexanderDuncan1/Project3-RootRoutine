84af287435d28aa80d6d141654605af3c4d42fee
AlexanderDuncan1 <hellstern@gmail.com>
2023-11-02 18:46:22 +1000

    gitignoreworking?

--- client/src/pages/loginForm.jsx
+++ client/src/pages/loginForm.jsx
@@ -1,9 +1,11 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import { Button, Form, Alert } from 'react-bootstrap';
 import { useNavigate } from 'react-router-dom';
+import { AuthContext } from '../Auth.jsx';
 
 function LoginForm() {
   const navigate = useNavigate();
+  const { login } = useContext(AuthContext);
   const [formData, setFormData] = useState({
     email: '',
     password: '',
@@ -12,8 +14,19 @@ function LoginForm() {
 
   const handleLogin = async (e) => {
     e.preventDefault();
-    navigate('/account');
-  }
+    try {
+        const response = await login(formData);
+
+        if (response?.success) {
+            navigate('/account');
+        } else {
+            setErrorMessage(response?.message || "Invalid credentials.");
+        }
+    } catch (error) {
+        setErrorMessage("An error occurred during login. Please try again.");
+    }
+}
+
 
   const handleChange = (e) => {
     setFormData({
@@ -25,6 +38,29 @@ function LoginForm() {
 
   return (
     <Form onSubmit={handleLogin}>
+      <Form.Group controlId="formBasicEmail">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control 
+          type="email" 
+          name="email" 
+          placeholder="Enter email" 
+          value={formData.email} 
+          onChange={handleChange}
+          aria-label="Email Address"
+        />
+      </Form.Group>
+
+      <Form.Group controlId="formBasicPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control 
+          type="password" 
+          name="password" 
+          placeholder="Password" 
+          value={formData.password} 
+          onChange={handleChange}
+          aria-label="Password"
+        />
+      </Form.Group>
 
       <Button variant="primary" type="submit">
         Login
